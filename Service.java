10684921ec61d0a2062388d2bda698199816e012
Likith Kumar S <101611090+Skllit@users.noreply.github.com>
2025-07-21 23:14:45 +0530

    Update Service.java

--- Service.java
+++ Service.java
@@ -35,6 +35,44 @@ public Boolean delete(int id){
 return books.removeIf(b->b.getId()===id);
 }
 ----------------------------------------------------------------------------
+
+  student -- id,name,age;
+
+service
+private final List<student>students=new ArrayList<>();
+int counter=1;
+
+//add
+public Student Add(Student student){
+student.setId(counter++)
+students.push(student);
+return student;
+}
+
+//getall
+public List<Student> Getall(){
+return students;
+}
+//getbyid
+public Optional<Student>GetbyId(int id){
+ return students.stream().filter(s->s.getId()==id).findFirst();
+
+}
+//update
+public Optional<Student> update(int id,Student student){
+return getByid(id).map(s->{
+s.setName(student.getName())
+s.setAge(student.getAge())
+return s;
+})
+}
+
+//delete
+public Boolean Delete(int id){
+return student.removeIF(s->s.getID()==id)
+}
+
+-------------------------------------------------------------------------------------------
   
 
 
